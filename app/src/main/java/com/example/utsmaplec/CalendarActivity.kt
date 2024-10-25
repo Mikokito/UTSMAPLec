c4a1835856cbc9fe3c48fa4b9b0da7a682f4bda0
Miko <citranandariani@gmail.com>
2024-10-25 22:05:50 +0700

    Test

--- app/src/main/java/com/example/utsmaplec/CalendarActivity.kt
+++ app/src/main/java/com/example/utsmaplec/CalendarActivity.kt
@@ -12,12 +12,7 @@ class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_calendar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         // Bottom Navigation View setup
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
